ccd69a8ad5faebf6e3dbbc5dbe0de13da954909e
Agus Riady <riadyagus1@gmail.com>
2021-07-09 22:02:46 +0700

    Login and Register Use Firebase

---
+++ Monday/LoginViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LoginViewController.swift
+//  Monday
+//
+//  Created by Agus Riady on 09/07/21.
+//
+
+import UIKit
+import Firebase
+
+@objc(EmailViewController)
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+      self.view.endEditing(true)
+    }
+        
+    @IBAction func loginBtn(_ sender: UIButton) {
+        let email = emailField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Signing in the user
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            
+            if error != nil {
+                // Couldn't sign in
+                let dialogMessage = UIAlertController(title: "Login Gagal", message: "\n Username / Password salah! Silahkan coba lagi!", preferredStyle: .alert)
+                // Create OK button with action handler
+                let ok = UIAlertAction(title: "Kembali", style: .default, handler: { (action) -> Void in
+                    print("Password salah Ok button tapped")
+                 })
+                
+                //Add OK button to a dialog message
+                dialogMessage.addAction(ok)
+                // Present Alert to
+                self.present(dialogMessage, animated: true, completion: nil)
+            }
+            else {
+                
+                let storyBoard:UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+                let newViewController = storyBoard.instantiateViewController(withIdentifier: "HomeViewController")
+                newViewController.modalPresentationStyle = .fullScreen
+                self.show(newViewController, sender: self)
+            }
+        }
+    }
+}
---
+++ Monday/SignUpViewController.swift
@@ -0,0 +1,111 @@
+//
+//  SignUpViewController.swift
+//  Monday
+//
+//  Created by Agus Riady on 09/07/21.
+//
+
+import UIKit
+import FirebaseAuth
+import Firebase
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func validateFields() -> String? {
+        
+        // Check that all fields are filled in
+        if nameField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            emailField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return "Please fill in all fields."
+        }
+        
+        return nil
+    }
+    
+
+    @IBAction func signUpButton(_ sender: Any) {
+        // Validate the fields
+        let error = validateFields()
+        
+        if error != nil {
+            
+            // There's something wrong with the fields, show error message
+            showError(error!)
+        }
+        else {
+            
+            // Create cleaned versions of the data
+            let name = nameField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = emailField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            // Create the user
+            Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
+                
+                // Check for errors
+                if err != nil {
+                    
+                    // There was an error creating the user
+                    self.showError("Error creating user")
+                }
+                else {
+                    
+                    // User was created successfully, now store the first name and last name
+                    let db = Firestore.firestore()
+                    
+                    db.collection("users").addDocument(data: ["name":name, "email":email, "uid": result!.user.uid ]) { (error) in
+                        
+                        if error != nil {
+                            // Show error message
+                            self.showError("Error saving user data")
+                        }
+                    }
+                    
+                    // Transition to the home screen
+                    self.transitionToHome()
+                }
+                
+            }
+            
+            
+            
+        }
+    }
+    
+    func showError(_ message:String) {
+        
+        //MARK: Alert Salah Username/Pas
+        let dialogMessage = UIAlertController(title: "Sign Up Gagal", message: "\n Sign Up gagal dikarenakan: \n - Nama / E-Mail / Password Belum terisi. \n - Kekuatan password anda lemah. \n Silahkan coba lagi!", preferredStyle: .alert)
+        // Create OK button with action handler
+        let ok = UIAlertAction(title: "Kembali", style: .default, handler: { (action) -> Void in
+            print("Daftar gagal Ok button tapped")
+         })
+        
+        //Add OK button to a dialog message
+        dialogMessage.addAction(ok)
+        // Present Alert to
+        self.present(dialogMessage, animated: true, completion: nil)
+    }
+    
+    func transitionToHome() {
+        
+        let storyBoard:UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let newViewController = storyBoard.instantiateViewController(withIdentifier: "HomeViewController")
+        newViewController.modalPresentationStyle = .fullScreen
+        self.show(newViewController, sender: self)
+        
+    }
+
+}
